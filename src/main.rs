e1aa501c93ad13c62c48059f80f17955954d2c62
Jeffrey McAteer <jeffrey.p.mcateer@gmail.com>
2018-10-02 17:40:14 -0400

    oh boy

---
+++ src/main.rs
@@ -0,0 +1,151 @@
+extern crate tsplib;
+use tsplib::{EdgeWeightType, NodeCoord, Type};
+
+use std::fs::File;
+use std::path::Path;
+use std::io::{BufReader};
+use std::env;
+use std::io::prelude::*;
+
+fn usage() {
+  println!(r#"Usage: ./tsp-sol path/to/berlin52.tsp
+"#);
+}
+
+fn main() {
+  let args: Vec<_> = env::args().collect();
+  if args.len() < 2 {
+    usage();
+    return;
+  }
+  
+  let file_arg = args.get(1).unwrap();
+
+  if ! Path::new(file_arg).exists() {
+    println!("File does not exist: {}", file_arg);
+    return;
+  }
+
+  let file = match File::open(file_arg) {
+    Ok(f) => f,
+    Err(e) => {
+      println!("Cannot open {}: {}", file_arg, e);
+      return;
+    }
+  };
+  
+  // Use tsp lib to parse file
+  let instance = match tsplib::parse( BufReader::new(file) ) {
+    Ok(i) => i,
+    Err(e) => {
+      println!("Error parsing tsplib file {}: {}", file_arg, e);
+      return;
+    }
+  };
+  
+  let node_coordinates: Vec<(usize, f32, f32)> = match instance.node_coord {
+    Some(node_c) => match node_c {
+      NodeCoord::Two(vec_count_loc_loc) => vec_count_loc_loc,
+      NodeCoord::Three(_we_dont_care) => {
+        println!("3D TSP problems currently unsupported.");
+        return;
+      }
+    },
+    None => {
+      println!("Err: no coordinates found in {}", file_arg);
+      return;
+    }
+  };
+  
+  // Compute 2x matrix of edge weights (assumes 2d euclidian geometry)
+  let mut weights: Vec<Vec<f32>> = vec![];
+  {
+    for row_r in &node_coordinates {
+      let mut row_weight_v: Vec<f32> = vec![];
+      for col_r in &node_coordinates {
+        let weight: f32 = (
+          (row_r.1 - col_r.1).powf(2.0) + // x1 + x2 squared
+          (row_r.2 - col_r.2).powf(2.0)   // y1 + y2 squared
+        ).sqrt();
+        
+        row_weight_v.push(weight);
+      }
+      weights.push(row_weight_v);
+    }
+  }
+  
+  println!("City has {} points", weights.len());
+  // remember weights is 2d square matrix (could be triangle, meh.)
+  
+  // If we have 3 or fewer points, we're done. min bound is O(1), good job folks.
+  if weights.len() <= 3 {
+    print!("Ideal trip order: ");
+    for i in 0..weights.len() {
+      print!("{}, ", i);
+    }
+    println!("");
+    return;
+  }
+  
+  // We begin with points 0, 1, and 2.
+  // These will be overwritten in the largest-triangle-fining process
+  let mut ordered_visits: Vec<usize> = vec![0, 1, 2]; // holds the path as a vector of indexes relating to the city number beginning at 0
+  
+  { // Find largest triangle
+    { // Make the first 2 points the furthest away in the entire graph
+      for r in 0..weights.len() {
+        for c in 0..weights.len() {
+          if r == c { continue; }
+          let best_largest_w = weights[ordered_visits[0]][ordered_visits[1]];
+          let this_largest_w    = weights[r][c];
+          if this_largest_w > best_largest_w {
+            ordered_visits[0] = r;
+            ordered_visits[1] = c;
+          }
+        }
+      }
+    }
+    { // Ensure ordered_visits[2] != ordered_visits[0] or ordered_visits[1]
+      while ordered_visits[2] == ordered_visits[0] || ordered_visits[2] == ordered_visits[1] {
+        ordered_visits[2] = (ordered_visits[2]+1) % weights.len();
+      }
+    }
+    { // Given the longest edge, find 
+      // weight(0, 2) + weight(1, 2) (weights of both edges going to "2")
+      let mut current_longest_point_len = weights[ordered_visits[0]][ordered_visits[2]] + weights[ordered_visits[1]][ordered_visits[2]];
+      for r in 0..weights.len() {
+        if r == ordered_visits[0] || r == ordered_visits[1] { continue; }
+        let this_len = weights[ordered_visits[0]][r] + weights[ordered_visits[1]][r];
+        if this_len > current_longest_point_len {
+          ordered_visits[2] = r;
+          current_longest_point_len = this_len;
+        }
+      }
+    }
+  }
+  
+  while ordered_visits.len() < weights.len() {
+    
+  }
+  
+  { // Print solution
+    println!("Solution distance: {}", compute_dist(&weights, &ordered_visits));
+    print!("Solution order: ");
+    for p in ordered_visits {
+      print!("{} ", p);
+    }
+    println!("");
+  }
+}
+
+fn compute_dist(weights: &Vec<Vec<f32>>, path: &Vec<usize>) -> f32 {
+  let mut total: f32 = 0.0;
+  for p_i in 0..path.len() {
+    total += weights[p_i][(p_i+1) % path.len()]; // mod lets us wrap at end (p_i == len(), (p_i+1) % len == 0)
+  }
+  return total;
+}
+
+
+
+
