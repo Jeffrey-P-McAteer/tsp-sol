648fcf5c30edfa34d03c6b326f406d6f86a1b9a8
Jeffrey McAteer <jeffrey.p.mcateer@gmail.com>
2018-10-02 20:39:39 -0400

    why did that break

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
 extern crate tsplib;
 use tsplib::{EdgeWeightType, NodeCoord, Type};
 
+extern crate image;
+use image::{ImageBuffer, Rgb};
+
+extern crate imageproc;
+use imageproc::drawing::draw_line_segment_mut;
+
 use std::fs::File;
 use std::path::Path;
 use std::io::{BufReader};
@@ -153,11 +159,14 @@ fn main() {
   { // Print solution
     println!("Solution distance: {}", compute_dist(&weights, &ordered_visits));
     print!("Solution order: ");
-    for p in ordered_visits {
-      print!("{} ", p);
+    for p in &ordered_visits {
+      print!("{} ", *p);
     }
     println!("");
   }
+  
+  save_state_image("./out.png", &ordered_visits, &node_coordinates, &center);
+  
 }
 
 fn compute_dist(weights: &Vec<Vec<f32>>, path: &Vec<usize>) -> f32 {
@@ -204,6 +213,7 @@ fn compute_furthest(path: &Vec<usize>, unordered: &Vec<usize>, weights: &Vec<Vec
       (center.1/*y*/ - locations[unord_elm].2/*y*/).powf(2.0)
     ).sqrt();
     if this_dist_from_center > furthest_i_dist_from_center {
+      furthest_i_dist_from_center = this_dist_from_center;
       furthest_i = unord_elm;
       unordered_idx = i;
       // we don't know where it is GOING yet.
@@ -233,3 +243,88 @@ fn compute_furthest(path: &Vec<usize>, unordered: &Vec<usize>, weights: &Vec<Vec
   
   return (furthest_i, path_idx, unordered_idx);
 }
+
+fn save_state_image<I: Into<String>>(file_path: I, path: &Vec<usize>, locations: &Vec<(usize, f32, f32)>, center: &(f32, f32)) {
+  let file_path = file_path.into();
+  let (width, height) = (1200, 1200);
+  let mut image = ImageBuffer::<Rgb<u8>, Vec<u8>>::new(width, height); // width, height
+  
+  let (smallest_x, largest_y, largest_x, smallest_y) = get_point_extents(locations);
+  let x_range: f32 = largest_x - smallest_x;
+  let y_range: f32 = largest_y - smallest_y;
+  
+  for loc in locations {
+    let (loc_x,loc_y) = scale_xy(width, height, x_range as u32, y_range as u32, smallest_x, smallest_y, loc.1, loc.2);
+    
+    // Set all location pixels to be red // r,g,b
+    //image.get_pixel_mut(loc_x, loc_y).data = [255, 0, 0];
+    circle_it(&mut image, loc_x, loc_y, [255, 0, 0]);
+    
+  }
+  
+  for pt_from in path {
+    let pt_to = (pt_from+1) % locations.len();
+    
+    let from_loc = locations[*pt_from];
+    let (from_loc_x,from_loc_y) = scale_xy(width, height, x_range as u32, y_range as u32, smallest_x, smallest_y, from_loc.1, from_loc.2);
+    
+    let to_loc = locations[pt_to];
+    let (pt_to_x,pt_to_y) = scale_xy(width, height, x_range as u32, y_range as u32, smallest_x, smallest_y, to_loc.1, to_loc.2);
+    
+    draw_line_segment_mut(&mut image,
+      (pt_to_x as f32,pt_to_y as f32), // start
+      (from_loc_x as f32,from_loc_y as f32), // end
+      Rgb([255, 255, 255])
+    );
+  }
+  
+  image.save(file_path).unwrap();
+}
+
+fn scale_xy(img_w: u32, img_h: u32, path_w: u32, path_h: u32, path_x_smallest: f32, path_y_smallest: f32, given_x: f32, given_y: f32) -> (u32, u32) {
+  let mut img_x = (given_x - path_x_smallest) * ((img_w as f32 / path_w as f32) as f32);
+  let mut img_y = (given_y - path_y_smallest) * ((img_h as f32 / path_h as f32) as f32);
+  return (img_x as u32, img_y as u32);
+}
+
+// erm they're actually squares. Whoops.
+fn circle_it(image: &mut ImageBuffer::<Rgb<u8>, Vec<u8>>, x: u32, y: u32, rgb: [u8; 3]) {
+  let r = 10; // radius
+  
+  let x: i32 = x as i32; // UGH
+  let y: i32 = y as i32;
+  
+  for x_off in (x-r)..(x+r) {
+    for y_off in (y-r)..(y+r) {
+      if x_off > 0 && x_off < image.width() as i32 && y_off > 0 && y_off < image.height() as i32 {
+        image.get_pixel_mut(x_off as u32, y_off as u32).data = rgb;
+      }
+    }
+  }
+}
+
+// returns smallestX, largestY, largestX, smallestY
+fn get_point_extents(locations: &Vec<(usize, f32, f32)>) -> (f32, f32, f32, f32) {
+  let mut smallest_x = f32::INFINITY;
+  let mut largest_y = f32::NEG_INFINITY;
+  let mut largest_x = f32::NEG_INFINITY;
+  let mut smallest_y = f32::INFINITY;
+  for loc in locations {
+    let x = loc.1;
+    let y = loc.2;
+    if x < smallest_x {
+      smallest_x = x;
+    }
+    if x > largest_x {
+      largest_x = x;
+    }
+    if y < smallest_y {
+      smallest_y = y;
+    }
+    if y > largest_y {
+      largest_y = y;
+    }
+  }
+  return (smallest_x, largest_y, largest_x, smallest_y);
+}
+
