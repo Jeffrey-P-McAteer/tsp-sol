bd12d9e65671c11b3b57e87a566acdd7c283ca35
Jeffrey McAteer <jeffrey.p.mcateer.civ@mail.mil>
2023-10-17 13:09:30 -0400

    Untested brute force equation

--- src/parabolics.rs
+++ src/parabolics.rs
@@ -68,20 +68,12 @@ impl System for Parabola {
     }
 }
 
-
 */
 
 
-pub fn solve_for_6pts(
-    (x1, y1): (fp, fp),
-    (x2, y2): (fp, fp),
-    (x3, y3): (fp, fp),
-    (x4, y4): (fp, fp),
-    (x5, y5): (fp, fp),
-    (x6, y6): (fp, fp),
-)
-    -> (fp, fp, fp, fp, fp, fp)
-{
+/*
+// OLD solve_for_6pts gaussian
+
     // Adapted from https://github.com/nerdypepper/gauss_jordan/blob/master/src/main.rs
     // Solve Ax^2 + Bxy + Cy^2 + Dx + Ey + F == 0
     const SIZE: usize = 6;
@@ -150,6 +142,73 @@ pub fn solve_for_6pts(
         system[0][SIZE], system[1][SIZE], system[2][SIZE],
         system[3][SIZE], system[4][SIZE], system[5][SIZE]
     );
+
+*/
+
+// Borrowed from https://stackoverflow.com/questions/73356686/rust-for-loop-with-floating-point-types
+fn float_loop(start: fp, threshold: fp, step_size: fp) -> impl Iterator<Item = fp> {
+    std::iter::successors(Some(start), move |&prev| {
+        let next = prev + step_size;
+        (next < threshold).then_some(next)
+    })
+}
+
+
+
+pub fn solve_for_6pts(
+    (x1, y1): (fp, fp),
+    (x2, y2): (fp, fp),
+    (x3, y3): (fp, fp),
+    (x4, y4): (fp, fp),
+    (x5, y5): (fp, fp),
+    (x6, y6): (fp, fp),
+)
+    -> (fp, fp, fp, fp, fp, fp)
+{
+    // We brute force everything, taking the smallest error (abs()'ing all Y values) for the 6 points
+    const RANGE_BEGIN: fp = -5.0;
+    const RANGE_END: fp = 5.0;
+    //const RANGE_STEP: fp = 0.1;
+    const RANGE_STEP: fp = 1.0;
+
+    let mut best_abcdef = (0.0, 0.0, 0.0, 0.0, 0.0, 0.0);
+    let mut smallest_error = 99999999.0;
+
+    for a in float_loop(RANGE_BEGIN, RANGE_END, RANGE_STEP) {
+        for b in float_loop(RANGE_BEGIN, RANGE_END, RANGE_STEP) {
+            for c in float_loop(RANGE_BEGIN, RANGE_END, RANGE_STEP) {
+                for d in float_loop(RANGE_BEGIN, RANGE_END, RANGE_STEP) {
+                    for e in float_loop(RANGE_BEGIN, RANGE_END, RANGE_STEP) {
+                        for f in float_loop(RANGE_BEGIN, RANGE_END, RANGE_STEP) {
+                            // On top of the world, baby
+                            
+                            let c_y1 = evaluate_parabolic_for_x_absonly(x1, (a,b,c,d,e,f));
+                            let c_y2 = evaluate_parabolic_for_x_absonly(x2, (a,b,c,d,e,f));
+                            let c_y3 = evaluate_parabolic_for_x_absonly(x3, (a,b,c,d,e,f));
+                            let c_y4 = evaluate_parabolic_for_x_absonly(x4, (a,b,c,d,e,f));
+                            let c_y5 = evaluate_parabolic_for_x_absonly(x5, (a,b,c,d,e,f));
+                            let c_y6 = evaluate_parabolic_for_x_absonly(x6, (a,b,c,d,e,f));
+                            
+                            let this_error = (c_y1 - y1).abs() +
+                                             (c_y2 - y2).abs() +
+                                             (c_y3 - y3).abs() +
+                                             (c_y4 - y4).abs() +
+                                             (c_y5 - y5).abs() +
+                                             (c_y6 - y6).abs();
+                            
+                            if this_error < smallest_error {
+                                best_abcdef = (a, b, c, d, e, f); // store lowest error!
+                                smallest_error = this_error;
+                            }
+
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    return best_abcdef;
 }
 
 // Given an X value, return ALL y values for the coefficients. Possible results are
@@ -186,5 +245,28 @@ pub fn evaluate_parabolic_for_x(x: fp, (a, b, c, d, e, f): (fp, fp, fp, fp, fp, 
 
     return y_vals;
 }
+
+
+// Faster, incorrect version of evaluate_parabolic_for_x
+pub fn evaluate_parabolic_for_x_absonly(x: fp, (a, b, c, d, e, f): (fp, fp, fp, fp, fp, fp)) -> fp {
+    let mut y = 0.0;
+
+    if c != 0.0 {
+        y = (-(
+                ( ((b*x) + e).powf(2.0) - (4.0*c*( (x*((a*x) + d)) + f)) ).sqrt() + (b*x) + e
+                ) / (
+                    2.0*c
+            )).abs();
+    }
+
+    if c == 0.0 && ((b*x)+e) != 0.0 {
+        y = (
+            -( (x*( (a*x) + d )) + f ) / ( (b*x) + e )
+        ).abs();
+    }
+
+    return y;
+}
+
 
 
