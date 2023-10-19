ff04815e6685cd483763f65d9647db6940397769
Jeffrey McAteer <jeffrey@jmcateer.com>
2023-10-19 10:36:42 -0400

    plotting playgound kinda done

--- scripts/conic_playground.py
+++ scripts/conic_playground.py
@@ -48,12 +48,7 @@ def float_range(begin_f, end_f, step_f):
   yield end_f
 
 def all_conic_y_vals(x, coeficients):
-  a = coeficients[A]
-  b = coeficients[B]
-  c = coeficients[C]
-  d = coeficients[D]
-  e = coeficients[E]
-  f = coeficients[F]
+  a, b, c, d, e, f = coeficients[A], coeficients[B], coeficients[C], coeficients[D], coeficients[E], coeficients[F]
   if c != 0:
     try:
       yield -(
@@ -87,8 +82,8 @@ def main(args=sys.argv):
   root_h = 900
   bottom_ui_h = 150
 
-  coeficient_min = -100.0
-  coeficient_max = 100.0
+  coeficient_min = -50.0
+  coeficient_max = 50.0
 
   graph_x_min = -10.0
   graph_x_max = 10.0
@@ -109,8 +104,33 @@ def main(args=sys.argv):
   controls_frm = ttk.Frame(frm, padding=0)
   controls_frm.pack(side='bottom', fill='x', expand=True)
 
-  q_btn = ttk.Button(controls_frm, text='Quit', command=root.destroy)
-  q_btn.pack(side='right', fill='both', expand=False)
+  right_control_cols = ttk.Frame(controls_frm, padding=0)
+  right_control_cols.pack(side='right')
+
+  right_leftmost_rows = ttk.Frame(right_control_cols, padding=5)
+  right_leftmost_rows.pack(side='left')
+
+  formula_txt = tkinter.Text(right_leftmost_rows)
+  formula_txt.pack(side='top', fill='x', expand=False)
+
+  def reset_all():
+    nonlocal last_coef_sum
+    a_in.set(1)
+    b_in.set(1)
+    c_in.set(1)
+    d_in.set(1)
+    e_in.set(1)
+    f_in.set(1)
+    last_coef_sum = -1.0
+
+  right_rightmost_rows = ttk.Frame(right_control_cols, padding=5)
+  right_rightmost_rows.pack(side='right')
+
+  reset_btn = ttk.Button(right_rightmost_rows, text='Reset', command=reset_all)
+  reset_btn.pack(side='bottom', fill='both', expand=False)
+
+  q_btn = ttk.Button(right_rightmost_rows, text='Quit', command=root.destroy)
+  q_btn.pack(side='top', fill='both', expand=False)
 
   coeficients = [
     tkinter.DoubleVar(value=1.0),
@@ -133,13 +153,22 @@ def main(args=sys.argv):
     currently_redrawing = True
     try:
       #print(f'coeficients = {coeficients}')
-      coeficient_vals = [float(x.get()) for x in coeficients]
+      coeficient_vals = [round(float(x.get()), 2) for x in coeficients]
       got_new_coefs = abs(sum(coeficient_vals) - last_coef_sum) > 0.1
       if got_new_coefs:
         last_draw_resolution = 0.5
 
+        # Write formula to text box
+        a,b,c,d,e,f = coeficient_vals[A],coeficient_vals[B],coeficient_vals[C],coeficient_vals[D],coeficient_vals[E],coeficient_vals[F],
+        formula_str = f'({a}*(x^2)) + ({b}*x*y) + ({c}*(y^2)) + ({d}*x) + ({e}*y) + {f} = 0'
+        try:
+          formula_txt.delete('1.0', tkinter.END)
+        except:
+          pass
+        formula_txt.insert(tkinter.END, formula_str)
+
       if last_draw_resolution > graph_draw_resolution_stop:
-        print(f'coeficient_vals = {coeficient_vals}, last_draw_resolution = {last_draw_resolution}')
+        # print(f'coeficient_vals = {coeficient_vals}, last_draw_resolution = {last_draw_resolution}')
 
         last_draw_resolution /= 2.0 # Double resolution (until <= graph_draw_resolution_stop point)
 
@@ -164,12 +193,15 @@ def main(args=sys.argv):
     currently_redrawing = False
     root.after(50, redraw_canvas) # infinite redraw loop at 20 fps, but we avoid drawing if the sum of coefficients is within 0.01 of the last draw
 
-  sliders_col = ttk.Frame(controls_frm, padding=5)
-  sliders_col.pack(side='left')
+  sliders_two_col = ttk.Frame(controls_frm, padding=5)
+  sliders_two_col.pack(side='left', expand=False)
 
+  sliders_col1 = ttk.Frame(sliders_two_col, padding=5)
+  sliders_col1.pack(side='left', expand=False)
 
-  a_frm = ttk.Frame(sliders_col, padding=5)
-  a_frm.pack(side='bottom')
+  
+  a_frm = ttk.Frame(sliders_col1, padding=5)
+  a_frm.pack(side='top')
   
   a_label = ttk.Label(a_frm, text="A")
   a_label.pack(side='left')
@@ -179,9 +211,8 @@ def main(args=sys.argv):
   a_in.pack(side='right')
 
 
-
-  b_frm = ttk.Frame(sliders_col, padding=5)
-  b_frm.pack(side='bottom')
+  b_frm = ttk.Frame(sliders_col1, padding=5)
+  b_frm.pack(side='top')
   
   b_label = ttk.Label(b_frm, text="B")
   b_label.pack(side='left')
@@ -192,8 +223,8 @@ def main(args=sys.argv):
 
 
 
-  c_frm = ttk.Frame(sliders_col, padding=5)
-  c_frm.pack(side='bottom')
+  c_frm = ttk.Frame(sliders_col1, padding=5)
+  c_frm.pack(side='top')
   
   c_label = ttk.Label(c_frm, text="C")
   c_label.pack(side='left')
@@ -201,6 +232,44 @@ def main(args=sys.argv):
   c_in = ttk.Scale(c_frm, from_=coeficient_min, to=coeficient_max, orient=HORIZONTAL, length=300, variable=coeficients[C])
   c_in.set(1)
   c_in.pack(side='right')
+
+  sliders_col2 = ttk.Frame(sliders_two_col, padding=5)
+  sliders_col2.pack(side='right')
+
+
+  d_frm = ttk.Frame(sliders_col2, padding=5)
+  d_frm.pack(side='top')
+  
+  d_label = ttk.Label(d_frm, text="D")
+  d_label.pack(side='left')
+
+  d_in = ttk.Scale(d_frm, from_=coeficient_min, to=coeficient_max, orient=HORIZONTAL, length=300, variable=coeficients[D])
+  d_in.set(1)
+  d_in.pack(side='right')
+
+
+
+  e_frm = ttk.Frame(sliders_col2, padding=5)
+  e_frm.pack(side='top')
+  
+  e_label = ttk.Label(e_frm, text="E")
+  e_label.pack(side='left')
+
+  e_in = ttk.Scale(e_frm, from_=coeficient_min, to=coeficient_max, orient=HORIZONTAL, length=300, variable=coeficients[E])
+  e_in.set(1)
+  e_in.pack(side='right')
+
+
+  f_frm = ttk.Frame(sliders_col2, padding=5)
+  f_frm.pack(side='top')
+  
+  f_label = ttk.Label(f_frm, text="F")
+  f_label.pack(side='left')
+
+  f_in = ttk.Scale(f_frm, from_=coeficient_min, to=coeficient_max, orient=HORIZONTAL, length=300, variable=coeficients[F])
+  f_in.set(1)
+  f_in.pack(side='right')
+
 
 
   redraw_canvas()
