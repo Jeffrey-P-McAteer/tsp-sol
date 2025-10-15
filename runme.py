c795d9c968881c4b32d1b5655f79e678fc775035
Jeffrey McAteer <jeffrey@jmcateer.com>
2025-10-15 13:59:18 -0400

    mics

--- runme.py
+++ runme.py
@@ -23,8 +23,8 @@ if __name__ == '__main__':
     maybeset('PREF_GPU', 'none')
     #maybeset('PREF_GPU', 'nvidia')
 
-    #maybeset('USE_BRUTE_CACHE', 't')
-    maybeset('USE_BRUTE_CACHE', 'f')
+    maybeset('USE_BRUTE_CACHE', 't')
+    #maybeset('USE_BRUTE_CACHE', 'f')
 
     maybeset('TSP_INITIAL_COORDS', '3.15,5.86 8.09,7.39 10.18,5.37 4.70,6.81 8.09,8.35 11.11,11.05 10.71,11.85 4.80,6.96 9.89,11.42 5.81,6.90 10.26,11.59')
 
@@ -33,7 +33,7 @@ if __name__ == '__main__':
 
       #subprocess.run('cargo run --release -- selective 12 4'.split())
 
-      subprocess.run('cargo run --release -- spray 11 0.5'.split())
+      subprocess.run('cargo run --release -- spray 11 0.1'.split())
 
     except:
       if not 'KeyboardInterrupt' in traceback.format_exc():
